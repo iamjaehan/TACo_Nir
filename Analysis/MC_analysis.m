671d92e963ec4a41f29ea1ed2a8ee5a040314e7d
iamjaehan <121158685+iamjaehan@users.noreply.github.com>
2024-09-03 14:10:49 -0500

    coflict resolve

--- Analysis/MC_analysis.m
+++ Analysis/MC_analysis.m
@@ -23,7 +23,7 @@ ylabel("Optimality gap",'FontName','Times','FontSize',18)
 title("Monte-Carlo test (4-player)","FontName",'Times','FontSize',18)
 ylim([0 inf])
 xlabel("Step size",'FontName','Times','FontSize',18)
-saveas(gcf,"resultPlot.png")
+saveas(gcf,"StepSize_OptimalityGap.png")
 
 
 figure(3)
@@ -39,4 +39,29 @@ ylabel("Iteration Steps",'FontName','Times','FontSize',18)
 title("Monte-Carlo test (4-player) - Convergence","FontName",'Times','FontSize',18)
 ylim([0 inf])
 xlabel("Step size",'FontName','Times','FontSize',18)
-saveas(gcf,"resultPlot.png")
+saveas(gcf,"StepSize_IterationSteps.png")
+
+figure(4)
+clf
+ss = size(data);
+% ll = 1:ss(1); ll=ll';
+ll = -1:3; ll = ll';
+ID = [];
+for i = 1:ss(2)
+    ID= horzcat(ID,ll);
+end
+
+delIdx = find(count < 1000);
+data = data(delIdx);
+count = count(delIdx);
+ID = ID(delIdx);
+
+scatter(data(:),count(:),50,ID(:))
+grid on
+xlabel("Optimality gap",'FontName',"Times",'FontSize',18)
+ylabel("Iteration Steps",'FontName',"Times",'FontSize',18)
+legend
+colormap("jet")
+colorbar()
+title("Tradeoff relationship","FontName",'Times','FontSize',18)
+saveas(gcf,"OptimalityGap_IterationSteps_tradeoff.png")
