39123a67dd586acf2989b5f49453c0f69de5cb6c
iamjaehan <121158685+iamjaehan@users.noreply.github.com>
2024-09-10 23:42:57 -0500

    experiment code

--- devel/MC_experiment.jl
+++ devel/MC_experiment.jl
@@ -1,2 +1,17 @@
 using datfm
 using MAT
+
+include("World.jl")
+include("0_GameSetup.jl")
+
+iterNum = 100
+n = 4
+testCaseNum = 4
+
+Record = Array{Any,2}(undef,testCaseNum,iterNum)
+OptGap = deepcopy(Record)
+Fairness = deepcopy(Record)
+Count = deepcopy(Record)
+
+for i = 1:itermNum
+end
